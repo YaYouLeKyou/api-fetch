75192af0eb0dd2efb0286cb56c2ff4df8f628cbf
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2021-05-19 16:15:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector("#cp").addEventListener("input", function () {
+  if (this.value.length == 5) {
+    let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=codesPostaux,population&format=json&geometry=centre`;
+
+    fetch(url)
+      .then((response) =>
+        response.json().then((data) => {
+          //   console.log(data);
+          let affichage = "<ul>";
+          for (let ville of data) {
+            affichage += `<li><strong>${ville.nom}</strong> - ${ville.population} habitants</li>`;
+          }
+          affichage += "</ul>";
+          document.querySelector("#villes").innerHTML = affichage;
+        })
+      )
+      .catch((err) => console.log("Erreur : " + err));
+  }
+});
